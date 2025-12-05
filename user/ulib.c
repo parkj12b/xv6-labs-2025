149869c4775738db3d00b299e6a1d92228d59c49
Minseo Park <jacob.park.9436@gmail.com>
2025-12-05 16:58:06 +0900

    refactor: clean up regex matcher implementation in ulib.c

--- user/ulib.c
+++ user/ulib.c
@@ -1,136 +1,114 @@
-#include "kernel/types.h"
-#include "kernel/stat.h"
 #include "kernel/fcntl.h"
 #include "kernel/riscv.h"
+#include "kernel/stat.h"
+#include "kernel/types.h"
 #include "kernel/vm.h"
 #include "user/user.h"
 
 //
 // wrapper so that it's OK if main() does not call exit().
 //
-void
-start()
-{
+void start() {
   extern int main();
   main();
   exit(0);
 }
 
-char*
-strcpy(char *s, const char *t)
-{
+char *strcpy(char *s, const char *t) {
   char *os;
 
   os = s;
-  while((*s++ = *t++) != 0)
+  while ((*s++ = *t++) != 0)
     ;
   return os;
 }
 
-int
-strcmp(const char *p, const char *q)
-{
-  while(*p && *p == *q)
+int strcmp(const char *p, const char *q) {
+  while (*p && *p == *q)
     p++, q++;
   return (uchar)*p - (uchar)*q;
 }
 
-uint
-strlen(const char *s)
-{
+uint strlen(const char *s) {
   int n;
 
-  for(n = 0; s[n]; n++)
+  for (n = 0; s[n]; n++)
     ;
   return n;
 }
 
-void*
-memset(void *dst, int c, uint n)
-{
-  char *cdst = (char *) dst;
+void *memset(void *dst, int c, uint n) {
+  char *cdst = (char *)dst;
   int i;
-  for(i = 0; i < n; i++){
+  for (i = 0; i < n; i++) {
     cdst[i] = c;
   }
   return dst;
 }
 
-char*
-strchr(const char *s, char c)
-{
-  for(; *s; s++)
-    if(*s == c)
-      return (char*)s;
+char *strchr(const char *s, char c) {
+  for (; *s; s++)
+    if (*s == c)
+      return (char *)s;
   return 0;
 }
 
-char*
-gets(char *buf, int max)
-{
+char *gets(char *buf, int max) {
   int i, cc;
   char c;
 
-  for(i=0; i+1 < max; ){
+  for (i = 0; i + 1 < max;) {
     cc = read(0, &c, 1);
-    if(cc < 1)
+    if (cc < 1)
       break;
     buf[i++] = c;
-    if(c == '\n' || c == '\r')
+    if (c == '\n' || c == '\r')
       break;
   }
   buf[i] = '\0';
   return buf;
 }
 
-int
-stat(const char *n, struct stat *st)
-{
+int stat(const char *n, struct stat *st) {
   int fd;
   int r;
 
   fd = open(n, O_RDONLY);
-  if(fd < 0)
+  if (fd < 0)
     return -1;
   r = fstat(fd, st);
   close(fd);
   return r;
 }
 
-int
-atoi(const char *s)
-{
+int atoi(const char *s) {
   int n;
 
   n = 0;
-  while('0' <= *s && *s <= '9')
-    n = n*10 + *s++ - '0';
+  while ('0' <= *s && *s <= '9')
+    n = n * 10 + *s++ - '0';
   return n;
 }
 
-void*
-memmove(void *vdst, const void *vsrc, int n)
-{
+void *memmove(void *vdst, const void *vsrc, int n) {
   char *dst;
   const char *src;
 
   dst = vdst;
   src = vsrc;
   if (src > dst) {
-    while(n-- > 0)
+    while (n-- > 0)
       *dst++ = *src++;
   } else {
     dst += n;
     src += n;
-    while(n-- > 0)
+    while (n-- > 0)
       *--dst = *--src;
   }
   return vdst;
 }
 
-int
-memcmp(const void *s1, const void *s2, uint n)
-{
+int memcmp(const void *s1, const void *s2, uint n) {
   const char *p1 = s1, *p2 = s2;
   while (n-- > 0) {
     if (*p1 != *p2) {
@@ -142,19 +120,52 @@ memcmp(const void *s1, const void *s2, uint n)
   return 0;
 }
 
-void *
-memcpy(void *dst, const void *src, uint n)
-{
+void *memcpy(void *dst, const void *src, uint n) {
   return memmove(dst, src, n);
 }
 
-char *
-sbrk(int n) {
-  return sys_sbrk(n, SBRK_EAGER);
+char *sbrk(int n) { return sys_sbrk(n, SBRK_EAGER); }
+
+char *sbrklazy(int n) { return sys_sbrk(n, SBRK_LAZY); }
+
+// Regexp matcher from Kernighan & Pike,
+// The Practice of Programming, Chapter 9, or
+// https://www.cs.princeton.edu/courses/archive/spr09/cos333/beautiful.html
+
+int matchstar(int c, char *re, char *text);
+int matchhere(char *re, char *text);
+
+// matchstar: search for c*re at beginning of text
+int matchstar(int c, char *re, char *text)
+{
+  do{  // a * matches zero or more instances
+    if(matchhere(re, text))
+      return 1;
+  }while(*text!='\0' && (*text++==c || c=='.'));
+  return 0;
 }
 
-char *
-sbrklazy(int n) {
-  return sys_sbrk(n, SBRK_LAZY);
+// matchhere: search for re at beginning of text
+
+int matchhere(char *re, char *text)
+{
+  if(re[0] == '\0')
+    return 1;
+  if(re[1] == '*')
+    return matchstar(re[0], re+2, text);
+  if(re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if(*text!='\0' && (re[0]=='.' || re[0]==*text))
+    return matchhere(re+1, text+1);
+  return 0;
 }
 
+int match(char *re, char *text) {
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do { // must look at empty string
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
